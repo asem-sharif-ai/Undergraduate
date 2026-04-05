e2e982e7fa47f9e3baa9572b92b107f3720b797e
Asem Sharif <asem.sharif.ai@gmail.com>
2026-04-05 10:17:01 +0200

    Delete Year-1 directory

--- Year-1/[PyGame] RGBA Palette Generator/Main.py
+++
@@ -1,9 +0,0 @@
-from GeneratePALETTE import *
-
-# Palette Map: ['RGB' 'RGBA' 'R' 'G' 'B' 'W' 'K' 'RG' 'GB' 'RB' 'BW' 'FAV']
-P = 'FAV'
-
-# Palette Window Length (Recommended: [551:751])
-L = 650
-
-generate(PALETTE, P, L)
